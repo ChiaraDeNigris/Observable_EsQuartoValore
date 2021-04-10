ba57248b8cd00b5ba98288bf901f8f538136a42b
ChiaraDeNigris <c.denigris@studenti.unipi.it>
2021-04-10 10:40:43 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { ajax } from 'rxjs/ajax';
+import { ajax } from "rxjs/ajax";
 import { Observable, interval } from "rxjs";
 
 const apiKey = "5c546389656e9425c3eb5ec97f1a0188";
@@ -6,20 +6,30 @@ const URL =
   "https://api.openweathermap.org/data/2.5/weather?APPID=" +
   apiKey +
   "&units=metric&q=";
-var city="Pisa";
-const tick = interval(10000);
+var city = "Pisa";
+const tick = interval(100);
+var conta = 0;
 //Costruisco l'observable
-const temp = new Observable(subscriber => tick.subscribe( { 
-  next() { 
-    fetch(URL + city)
-      .then(response =>  response.json())
-        .then(data => 
-          subscriber.next(data.main.temp));
-  } } ) ); 
+const temp = new Observable(subscriber =>
+  tick.subscribe({
+    next() {
+      while (conta < 4) {
+        fetch(URL + city)
+          .then(response => response.json())
+          .then(data => subscriber.next(data.main.temp));
+        conta += 1;
+      }
+    }
+  })
+);
 // Due subscriber
-temp.subscribe({   
-  next(x) {console.log(x);}
-})
-temp.subscribe({   
-  next(x) {document.getElementById("output").innerHTML+=x+"<br>";}
-})
+temp.subscribe({
+  next(x) {
+    console.log(x);
+  }
+});
+temp.subscribe({
+  next(x) {
+    document.getElementById("output").innerHTML += x + "<br>";
+  }
+});
